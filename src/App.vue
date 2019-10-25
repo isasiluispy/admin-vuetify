af3cf0ece151ddab500f1a65c045e53920305844
isasiluispy <isasiluispy@gmail.com>
2019-10-25 00:32:54 -0300

    improve layout by using just one source

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
-  <router-view></router-view>
+    <v-app>
+        <sidebar v-if="!isSimpleView" :drawer="drawer"/>
+        <navbar v-if="!isSimpleView" @changeDrawer="updateDrawer"/>
+        <v-content>
+            <v-container fluid :class="{'fill-height': isSimpleView}">
+                <router-view/>
+            </v-container>
+        </v-content>
+    </v-app>
 </template>
 
 <script>
+  import Navbar from "./components/Navbar";
+  import Sidebar from "./components/Sidebar";
 
-export default {
-  name: 'App',
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    name: 'App',
+    data: () => ({
+      drawer: true
+    }),
+    components: {
+      navbar: Navbar,
+      sidebar: Sidebar
+    },
+    methods: {
+      updateDrawer() {
+        this.drawer = !this.drawer;
+      }
+    },
+    computed: {
+      isSimpleView() {
+        return ['login'].indexOf(this.$route.name) > -1;
+      }
+    }
+  };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import DashboardLayout from "../layout/DashboardLayout";
 import Login from "../views/Login";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    redirect: 'dashboard',
-    component: DashboardLayout,
-    children: [
-      {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: () => import('../views/Dashboard.vue')
-      },
-    ]
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/Dashboard.vue')
   },
   {
     path: '/login',
